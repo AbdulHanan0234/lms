3c9172c49df4612dd2055fd1d91f5ffc778b8bd5
Abdul Hanan <ah24620055@gmail.com>
2026-07-14 17:25:14 +0500

    style: update background size and spacing in LoginPage component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import kfueitlogo from "../assets/kfueit-Logo.png";
 export const LoginPage = () => {
   return (
     <div className="login-page d-flex justify-content-center align-items-center vh-100">
-      <div className="col-3 login-box h-75 rounded">
+      <div className="col-3 login-box rounded">
         <div className="row login-logo justify-content-center pt-3 ">
           <img src={kfueitlogo} alt="logo" />
         </div>
@@ -22,27 +22,27 @@ export const LoginPage = () => {
           <input type="password" placeholder="Password" />
         </div>
 
-        <div className="row  login-test mt-4">
+        <div className="row  login-test mt-3">
           <p className="fs-5 col-7 text-center pt-2">4+7 = </p>
           <input type="number" className="col-3" />
         </div>
         
 
-        <div className="row text-center login-clue login-text fw-bold mt-3">
+        <div className="row text-center login-clue login-text fw-bold">
           <p>Example: If 2+2 = 4, then Write only 4 in the box</p>
         </div>
-        <div className="row  justify-content-center login-text login-btn  ">
+        <div className="row  justify-content-center login-text login-btn  mb-2">
           <button className="btn rounded-1 text-white">
             <i class="fa fa-sign-in pe-1 " aria-hidden="true"></i>
             <p className="d-inline">Login</p>
           </button>
         </div>
-        <div className="row text-center login-text bottom-text mt-3">
+        <div className="row text-center login-text bottom-text">
           <p>Reset Password</p>
         </div>
-        <div className="row text-center login-text ">
+        <div className="row text-center login-text mt-3">
           <p>
-            Copyright © 2026 Developed by{" "}
+            Copyright © 2026 Developed by
             <span className="bottom-text">KFUEIT</span>
           </p>
         </div>
